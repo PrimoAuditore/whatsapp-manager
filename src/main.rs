40412e2d0db35aa7e5c583f2f83dafcf22efd8ff
Ezio Tobioli <headlesshoudini@protonmail.com>
2023-01-01 23:13:23 -0300

    added time dependency, updated ingress to include merge request deploy, updated handlers to new distribution systems

--- src/main.rs
+++ src/main.rs
@@ -96,30 +96,6 @@ async fn validate(validation_parameters: HttpRequest) -> impl Responder {
 }
 
 
-#[get("/webhook")]
-async fn validate(validation_parameters: HttpRequest) -> impl Responder {
-    let verify_token = match env::var("VERIFY_TOKEN") {
-        Ok(x) => x,
-        Err(err) => panic!("{}", err),
-    };
-
-
-    let mut param_map = HashMap::new();
-
-    for param in validation_parameters.query_string().split("&") {
-        let param_vec: Vec<&str> = param.split("=").collect();
-        param_map.insert(param_vec[0], param_vec[1]);
-    }
-
-    if verify_token != param_map.get("hub.verify_token").unwrap().to_string() {
-        panic!("Received verification token is not equals to defined one")
-    }
-
-    debug!("{:?}", &param_map);
-
-    HttpResponse::Ok().body(param_map.get("hub.challenge").unwrap().to_string())
-}
-
 #[post("/message")]
 async fn send_message(message: web::Json<MessageRequest>) -> impl Responder {
     let response = request_handler::send_message(message.0);
